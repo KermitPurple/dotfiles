0353d5576babc436b284e4e9330c6b138b9de2e4
Shane McDonough <shanemcdman@gmail.com>
2021-09-07 15:54:08 -0700

    Add ncurses flags to .ycm_extra_config.py

--- vim/.ycm_extra_conf.py
+++ vim/.ycm_extra_conf.py
@@ -7,11 +7,18 @@ def Settings(**kwargs):
             stdout=subprocess.PIPE
             )
     raylib_flags = raylib_process.stdout.read().decode('utf-8').split()
+    ncurses_process = subprocess.Popen(
+            '/bin/zsh -c "pkg-config --libs --cflags ncurses"',
+            shell=True,
+            stdout=subprocess.PIPE
+            )
+    ncurses_flags = raylib_process.stdout.read().decode('utf-8').split()
     print(raylib_flags)
     return {
         'flags': [
             '-xc++',
             '-Iinclude',
             '-Isrc',
-        ] + raylib_flags,
+        ] + raylib_flags
+        + ncurses_flags,
     }
