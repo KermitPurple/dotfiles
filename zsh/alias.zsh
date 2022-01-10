379c40edcf773a841d081ae66e66f8f6e46164ef
Shane McDonough <shanemcdman@gmail.com>
2022-01-09 18:48:29 -0800

    Add comments for repos in alias.zsh

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,11 +1,14 @@
+# git@github.com:kermitpurple/rename_all
 # built with:
 # cargo build --release
 alias rna="$CODING_DIR/rust/rename_all/target/release/rename_all"
 
+# git@github.com:KermitPurple/ConwaysGameOfLifeCpp.git
 # built with:
 # make clean all TARGET=bin/conway
 alias conway="silent_bg $CODING_DIR/c++/ConwaysGameOfLifeCpp/bin/conway"
 
+# git@github.com:KermitPurple/TesseractV2.git
 # built with:
 # make release
 alias tess="open -a XQuartz&&\
@@ -13,6 +16,7 @@ sleep 1&&\
 export DISPLAY=:0&&\
 silent_bg $CODING_DIR/c++/TesseractV2/bin/tess"
 
+# git@github.com:KermitPurple/raylib_fib.git
 # built with:
 # make clean all TARGET=bin/fib
 alias fib="silent_bg $CODING_DIR/c++/fib/bin/fib"
@@ -26,6 +30,7 @@ alias c++_make="clang++ */*.cpp -Iinclude -o\"$TEMP_DIR/test\" && \"$TEMP_DIR/te
 # reload the zshrc
 alias refresh=". ~/.zshrc"
 
+# git@github.com:KermitPurple/DrawShapes.git
 # shape drawing program
 alias shapes="silent_bg $CODING_DIR/c++/shapes/bin/shapes"
 
@@ -38,5 +43,6 @@ alias shapes="silent_bg $CODING_DIR/c++/shapes/bin/shapes"
 #     neofetch - brew install neofetch
 alias welcome="new_window \"clear && neofetch\" && new_window rain && clear && figlet \"Welcome Shane!\" | cowsay -n -fdragon | lolcat"
 
+# git@github.com:KermitPurple/roll_dice_py.git
 # dice roll function
 alias roll="/Users/shane/coding/python/dice/main.py"
